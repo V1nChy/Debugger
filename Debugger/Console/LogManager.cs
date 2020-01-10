13d3f28a4d29c50dceef40d9e5ebc4b803c57b52
V1nChy <593386778@qq.com>
2020-01-10 12:03:14 +0800

    Modify Log Class

--- Debugger/Console/LogManager.cs
+++ Debugger/Console/LogManager.cs
@@ -3,18 +3,18 @@ using LuaInterface;
 
 namespace GFW
 {
-    public class LogManager
+    public class LogMgr
     {
         private static bool m_UseLog = false;
         public static bool EnableLog
         {
             get
             {
-                return LogManager.m_UseLog;
+                return LogMgr.m_UseLog;
             }
             set
             {
-                LogManager.m_UseLog = value;
+                LogMgr.m_UseLog = value;
                 Debugger.useLog = value;
                 Debug.unityLogger.logEnabled = value;
             }
@@ -22,28 +22,28 @@ namespace GFW
 
         public static void Log(string tag, string methodName, string message)
         {
-            if (LogManager.EnableLog)
+            if (LogMgr.EnableLog)
             {
                 Debugger.Log(tag, methodName, message);
             }
         }
         public static void Log(string tag, string methodName, string str, params object[] param)
         {
-            if (LogManager.EnableLog)
+            if (LogMgr.EnableLog)
             {
                 Debugger.Log(tag, methodName, str, param);
             }
         }
         public static void Log(string message)
         {
-            if (LogManager.EnableLog)
+            if (LogMgr.EnableLog)
             {
                 Debugger.Log(message);
             }
         }
         public static void Log(string str, params object[] param)
         {
-            if (LogManager.EnableLog)
+            if (LogMgr.EnableLog)
             {
                 Debugger.Log(str, param);
             }
@@ -51,28 +51,28 @@ namespace GFW
 
         public static void LogWarning(string tag, string methodName, string message)
         {
-            if (LogManager.EnableLog)
+            if (LogMgr.EnableLog)
             {
                 Debugger.LogWarning(tag, methodName, message);
             }
         }
         public static void LogWarning(string tag, string methodName, string str, params object[] param)
         {
-            if (LogManager.EnableLog)
+            if (LogMgr.EnableLog)
             {
                 Debugger.LogWarning(tag, methodName, str, param);
             }
         }
         public static void LogWarning(string message)
         {
-            if (LogManager.EnableLog)
+            if (LogMgr.EnableLog)
             {
                 Debugger.LogWarning(message);
             }
         }
         public static void LogWarning(string str, params object[] param)
         {
-            if (LogManager.EnableLog)
+            if (LogMgr.EnableLog)
             {
                 Debugger.LogWarning(str, param);
             }
@@ -80,28 +80,28 @@ namespace GFW
 
         public static void LogError(string tag, string methodName, string message)
         {
-            if (LogManager.EnableLog)
+            if (LogMgr.EnableLog)
             {
                 Debugger.LogError(tag, methodName, message);
             }
         }
         public static void LogError(string tag, string methodName, string str, params object[] param)
         {
-            if (LogManager.EnableLog)
+            if (LogMgr.EnableLog)
             {
                 Debugger.LogError(tag, methodName, str, param);
             }
         }
         public static void LogError(string message)
         {
-            if (LogManager.EnableLog)
+            if (LogMgr.EnableLog)
             {
                 Debugger.LogError(message);
             }
         }
         public static void LogError(string str, params object[] param)
         {
-            if (LogManager.EnableLog)
+            if (LogMgr.EnableLog)
             {
                 Debugger.LogError(str, param);
             }
